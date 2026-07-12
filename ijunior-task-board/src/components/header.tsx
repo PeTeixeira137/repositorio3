92fe39f903c46a3febada8e27d8cba5c88063c09
Pedro Teixeira <s6551251@gmail.com>
2026-07-12 15:49:48 -0300

    feat: completa o código do App.tsx e adiciona os exemplos

--- ijunior-task-board/src/components/header.tsx
+++ ijunior-task-board/src/components/header.tsx
@@ -1,4 +1,4 @@
-export function header() {
+export function Header() {
     return (
         <header className="bg-gray-800 text-white p-4 shadow-md">
             <h1 className="text-xl font-bold">iRepair - Painel</h1>
