68403eb8aa9af4334573a4c80762c2cd81d06585
Pedro Teixeira <s6551251@gmail.com>
2026-07-12 15:26:21 -0300

    feat: cria o componente NewServiceForm, que cria novas OS e as adiciona na lista

--- ijunior-task-board/src/App.tsx
+++ ijunior-task-board/src/App.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { ServiceCard } from './components/ServiceCard';
+import type { OrdemServico } from './types';
+
+
 export function App() {
   return (
     <h1 className="text-3xl font-bold underline">
