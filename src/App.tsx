cbf9dcc598467f8834f29745d1837cc74beb466e
Pedro Teixeira <s6551251@gmail.com>
2026-07-17 17:52:11 -0300

    feat: Cria a interface do cliente em types/client.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import { useState } from 'react';
-import { Header } from './components/Header';
-import { ServiceCard } from './components/ServiceCard';
-import { NewServiceForm } from './components/NewServiceForm';
-import type { OrdemServico } from './types';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { DashboardPage } from './pages/DashboardPage';
+import { ClientsPage } from './pages/ClientsPage';
+import { ServiceOrdersPage } from './pages/ServiceOrdersPage';
 
-
-export function App() {
-  const [ordensServico, setOrdensServico] = useState<OrdemServico[]>([
-    {
-      id: 1,
-      nomeCliente: 'Pedro Farace',
-      modeloAparelho: 'Samsung A22 5G',
-      defeito: 'Tela quebrada',
-      status: 'aberto'
-    },
-    {
-      id: 2,
-      nomeCliente: 'Cecília',
-      modeloAparelho: 'Iphone 11',
-      defeito: 'Bateria não carrega',
-      status: 'em andamento'
-    },
-  ]);
-
-  function adicionarOrdemServico(novaOrdem: OrdemServico) {
-    setOrdensServico([...ordensServico, novaOrdem]);
-  }
-
+const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <main className="container mx-auto p-4">
-        <NewServiceForm onAddServico={adicionarOrdemServico} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {ordensServico.map((ordem) => (
-            <ServiceCard key={ordem.id} ordem={ordem} />
-          ))}
-        </div>
-      </main>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/clients" element={<ClientsPage />} />
+        <Route path="/service-orders" element={<ServiceOrdersPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
