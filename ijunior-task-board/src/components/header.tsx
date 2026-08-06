14a1e61e79d8378cd14be35aa4f98b090a8f2ead
Pedro Teixeira <s6551251@gmail.com>
2026-08-06 16:37:12 -0300

    chore: remove pasta ijunior-task-board duplicada e desatualizada

--- ijunior-task-board/src/components/header.tsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-    return (
-        <header className="bg-gray-800 text-white p-4 shadow-md">
-            <h1 className="text-xl font-bold">iRepair - Painel</h1>
-        </header>
-    )
-}
