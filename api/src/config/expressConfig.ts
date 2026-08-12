e09bd21ab44decf6b176ee4a3ae01e3c366b37f4
Pedro Teixeira <s6551251@gmail.com>
2026-08-12 15:29:13 -0300

    feat: completa o server.ts com a portae o import do app que faltava

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,11 +1,24 @@
-import cookieParser from 'cookie-parser'
+import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { authRoutes } from '../domains/auth/auth.routes'
+import { clientsRoutes } from '../domains/clients/clients.routes'
+import { serviceOrdersRoutes } from '../domains/service-orders/service-orders.routes'
+import { errorHandler } from '../middlewares/errorHandler'
+
+const app = express()
 
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }))
-
+app.use(express.json())
 app.use(cookieParser())
 
 app.use('/auth', authRoutes)
+app.use('/clients', clientsRoutes)
+app.use('/service-orders', serviceOrdersRoutes)
+
+app.use(errorHandler)
+
+export { app }
